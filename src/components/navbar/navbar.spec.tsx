1565372cec6e0902a9b60a648066712d3d3deed5
lucassodresa <lucas.sodresa@gmail.com>
2023-10-30 23:04:41 +0000

    wip: menu in progress

---
+++ src/components/navbar/navbar.spec.tsx
@@ -0,0 +1,11 @@
+// import { render, screen } from '@testing-library/react';
+// import { Navbar } from '.';
+
+describe('example', () => {
+  it('should render', () => {
+    // render(<Navbar />);
+    // const component = screen.getByTestId('example-container');
+
+    expect(1).toBe(1);
+  });
+});
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { useToggle } from '@mantine/hooks';
+import { NavbarBrand } from './navbar-brand';
+import { NavbarItems } from './navbar-items';
+import clsx from 'clsx';
+
+export const Navbar = () => {
+  const [isOpen, toggleOpen] = useToggle();
+
+  return (
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+      <nav
+        className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+        aria-label="Global"
+      >
+        <div className="flex items-center justify-between">
+          <NavbarBrand />
+          <div className="sm:hidden">
+            <button
+              type="button"
+              className="p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+              aria-controls="navbar-image-and-text-1"
+              aria-label="Toggle navigation"
+              onClick={() => toggleOpen()}
+            >
+              <svg
+                className={clsx('w-4 h-4', isOpen ? 'block' : 'hidden')}
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+              </svg>
+              <svg
+                className={clsx('w-4 h-4', isOpen ? 'hidden' : 'block')}
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <NavbarItems isOpen={isOpen} />
+      </nav>
+    </header>
+  );
+};
